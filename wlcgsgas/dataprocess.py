884927e3c92cd24ed050e19d8291b56fa64fd8fc
Henrik Thostrup Jensen <thostrup@gmail.com>
2011-02-18 13:45:38 +0100

    add functionality for tier merging

--- wlcgsgas/dataprocess.py
+++ wlcgsgas/dataprocess.py
@@ -27,8 +27,8 @@ KSI2K_WALL_TIME = 'ksi2k_wall_time'
 DATE_START  = 'date_start'
 DATE_END    = 'date_end'
 
-FIELDS = ( YEAR, MONTH, HOST, VO_NAME, VO_GROUP, VO_ROLE, USER, N_JOBS, CPU_TIME, WALL_TIME, KSI2K_CPU_TIME, KSI2K_WALL_TIME )
-KEY_FIELDS = ( YEAR, MONTH, HOST, VO_NAME, VO_GROUP, VO_ROLE, USER )
+FIELDS = ( YEAR, MONTH, TIER, HOST, VO_NAME, VO_GROUP, VO_ROLE, USER, N_JOBS, CPU_TIME, WALL_TIME, KSI2K_CPU_TIME, KSI2K_WALL_TIME )
+KEY_FIELDS = ( YEAR, MONTH, TIER, HOST, VO_NAME, VO_GROUP, VO_ROLE, USER )
 
 DEFAULT_SCALE_FACTOR = 1.75
 
@@ -104,6 +104,15 @@ def mergeRecords(records):
 
 
 
+def createFieldKey(record):
+    """
+    Returns the key fields of a record as a tuple.
+    """
+    key = tuple ( [ record[field] for field in KEY_FIELDS if field in record ] )
+    return key
+
+
+
 def collapseFields(records, collapse_fields):
     """
     Removes one or more key fields in the records and sums togther the records
@@ -118,7 +127,7 @@ def collapseFields(records, collapse_fields):
         r = rec.copy()
         for cf in collapse_fields:
             del r[cf]
-        key = tuple ( [ r[field] for field in KEY_FIELDS if field in r ] )
+        key = createFieldKey(rec)
         collapsed_records.setdefault(key, []).append(r)
 
     summed_records = []
@@ -127,4 +136,92 @@ def collapseFields(records, collapse_fields):
 
     return summed_records
 
+
+
+def tierMergeSplit(records, tier_mapping, tier_shares, default_tier):
+
+    def mapHostToTier(host):
+        try:
+            return str(tier_mapping[host])
+        except KeyError:
+            print "WARNING: No tier mapping for host %s" % host
+            return default_tier
+#        # using heuristic
+#        if host.endswith('.no'):
+#            tier = NORWAY_TIER2
+#        elif host.endswith('.se'):
+#            tier = SWEDEN_TIER2
+#        elif host.endswith('.fi'):
+#            tier = CSC_TIER
+#        else:
+#            tier = DEFAULT_TIER
+#        return tier
+
+
+    def ruleMatch(rule, record):
+        for key, value in rule.items():
+            try:
+                if record[key] != value:
+                    return False
+            except KeyError:
+                return False
+        return True
+
+
+    def applyRatio(record, ratio):
+        record[N_JOBS]          = int(record[N_JOBS]      * ratio)
+        record[CPU_TIME]        = record[CPU_TIME]        * ratio
+        record[WALL_TIME]       = record[WALL_TIME]       * ratio
+        record[KSI2K_CPU_TIME]  = record[KSI2K_CPU_TIME]  * ratio if KSI2K_CPU_TIME  in record else None
+        record[KSI2K_WALL_TIME] = record[KSI2K_WALL_TIME] * ratio if KSI2K_WALL_TIME in record else None
+        return record
+
+
+    # start of function
+    tr = {}
+
+    for r in records:
+
+        rc = r.copy()
+        rc[TIER] = mapHostToTier(r[HOST])
+        del rc[HOST]
+
+        match = False
+        for rule, ratio in tier_shares:
+            if ruleMatch(rule, rc):
+                match = True
+                break
+
+        # we don't split entries if there are less than 10 jobs,
+        # as it creates small meaningless entries, which are just noise
+        # also, if ratio is 0 or 1 skip the check as it is meaningless
+        if match and rc[N_JOBS] > 10:
+            #print "SPLIT", rc[TIER], rc[USERSN], rc[VO_NAME], rc[VO_GROUP], rc[VO_ROLE], rc[N_JOBS]
+            t2_ratio = ratio
+            t1_ratio = 1 - t2_ratio
+
+            if t1_ratio != 0:
+                t1_base_record = rc.copy()
+                t1_base_record.update( {TIER: default_tier} )
+
+                t1k = createFieldKey(t1_base_record)
+                t1_record = applyRatio(t1_base_record, t1_ratio)
+                tr.setdefault(t1k, []).append(t1_record)
+
+            if t2_ratio != 0:
+                t2k = createFieldKey(rc)
+                t2_record = applyRatio(rc, t2_ratio)
+                tr.setdefault(t2k, []).append(t2_record)
+
+        else:
+            r_key = createFieldKey(rc)
+            tr.setdefault(r_key, []).append( applyRatio(rc, 1) )
+
+
+    tier_records = []
+
+    for t_recs in tr.values():
+        tier_records.append( mergeRecords(t_recs) )
+
+    return tier_records
 
